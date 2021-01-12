5ad7651012b844c70771947b696e2e5e103b0e00
Rich Korneisel <korneisel@cfu.net>
2021-01-12 15:29:12 -0600

    Commit 1 Group Answer

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+let pushButton = document.querySelector('#set-color');
+pushButton.addEventListener('click', determineColor);
 
+
+function determineColor (event) {
+event.preventDefault();
+let colorValue = document.querySelector('#color-field').value;
+console.log (colorValue);
+document.querySelector('.brush').style.background = colorValue
+}
