008f9628a0d2f0b0ca06eda1064ec2219cd306d9
Rich Korneisel <korneisel@cfu.net>
2021-01-12 15:52:54 -0600

    end of lab time going to homework

--- pixart.js
+++ pixart.js
@@ -8,3 +8,9 @@ let colorValue = document.querySelector('#color-field').value;
 console.log (colorValue);
 document.querySelector('.brush').style.background = colorValue
 }
+let div = document.createElement('div');
+div.classList.add('square');
+document.body.appendChild(div);
+
+
+console.log(div)
